428fbb62adc3ed166ebb21086c7659e0e0354655
MS5682 <92199689+MS5682@users.noreply.github.com>
2023-09-21 16:05:30 +0900

    update

--- controllers/parkingController.js
+++ controllers/parkingController.js
@@ -2,8 +2,30 @@ const express = require('express');
 const parkingModel = require('../models/parkingModel');
 
 exports.getCarList = (req, res)=>{
+  const user_code = 0;
+  //user_code = req.session.user_code;
+  if(user_code == 0){
     parkingModel.getCarList()
-      .then((result) => {
+    .then((resultData) => {
+      const result = {
+        user_code: user_code,
+        result: resultData
+      };
+      //res.send({ result: result })
+      res.render('parking', { result: result });
+    })
+    .catch((error) => {
+      // Authentication failed
+      res.status(400).send("에러발생");
+    });
+  }
+  else if(user_code == 1){
+    parkingModel.getCarListDetail()
+      .then((resultData) => {
+        const result = {
+          user_code: user_code,
+          result: resultData
+        };
         //res.send({ result: result })
         res.render('parking', { result: result });
       })
@@ -11,4 +33,6 @@ exports.getCarList = (req, res)=>{
         // Authentication failed
         res.status(400).send("에러발생");
       });
-}
+
+    }
+  }
--- models/parkingModel.js
+++ models/parkingModel.js
@@ -8,12 +8,39 @@ const conn = mysql.createConnection({
   database: process.env.DB_DATABASE,
 });
 
+module.exports.getCarListDetail = () => {
+    return new Promise((resolve, reject) => {
+        let sql = 'SELECT\
+        section,\
+        section_number,\
+        floor,\
+        MAX(parking.car_num) AS car_num,\
+        COUNT(parking.car_num) AS car_num_exists,\
+        MAX(user.phone_number) AS phone_number,\
+        MAX(user.id) AS id\
+        FROM parking LEFT OUTER JOIN user\
+        ON parking.car_num = user.car_number\
+        WHERE parking.`exit` IS NULL\
+        GROUP BY section, section_number, floor;';
+        conn.query(sql, (err, rows, fields) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(rows);
+            }
+        });
+    });
+};
+
 module.exports.getCarList = () => {
     return new Promise((resolve, reject) => {
-        let sql = 'SELECT parking.*, user.user_code, user.phone_number, user.id\
+        let sql = 'SELECT section,\
+        section_number,\
+        floor,\
+        COUNT(car_num) AS car_num_exists\
         FROM parking\
-        LEFT OUTER JOIN user\
-        ON parking.car_num = user.car_number';
+        WHERE `exit` IS NULL\
+        GROUP BY section, section_number, floor;';
         conn.query(sql, (err, rows, fields) => {
             if (err) {
                 reject(err);
