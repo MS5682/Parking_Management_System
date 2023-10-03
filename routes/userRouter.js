692e2d13508a88f49a856779a4eb4606ef62f8b9
MS5682 <92199689+MS5682@users.noreply.github.com>
2023-10-03 23:54:59 +0900

    Update userRouter.js

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const userController = require('../controllers/userController');
-function checkSessionAndUserType(req, res, next) {
+function checkSession(req, res, next) {
     if (!req.session.user) {
       res.redirect('/user/login');
     } else {
@@ -12,14 +12,14 @@ function checkSessionAndUserType(req, res, next) {
 router.post('/join', userController.join);
 router.get('/login', function(req, res, next) {res.render('login');});
 router.post('/login', userController.adminLogin);
-router.get('/list', checkSessionAndUserType, userController.getUserList);
-router.get('/update/:id', checkSessionAndUserType, userController.getUserInfo);
-router.get('/updateCar/:id', checkSessionAndUserType, userController.getUserCarInfo);
-router.post('/update/:id', checkSessionAndUserType, userController.updateUserInfo);
-router.post('/delete/:id', checkSessionAndUserType, userController.deleteUserInfo);
-router.post('/updateCar/:current/:new', checkSessionAndUserType, userController.updateUserCar);
-router.post('/addCar/:id/:car_number', checkSessionAndUserType, userController.addUserCar);
-router.post('/deleteCar/:car_number', checkSessionAndUserType, userController.deleteUserCar);
+router.get('/list', checkSession, userController.getUserList);
+router.get('/update/:id', checkSession, userController.getUserInfo);
+router.get('/updateCar/:id', checkSession, userController.getUserCarInfo);
+router.post('/update/:id', checkSession, userController.updateUserInfo);
+router.post('/delete/:id', checkSession, userController.deleteUserInfo);
+router.post('/updateCar/:current/:new', checkSession, userController.updateUserCar);
+router.post('/addCar/:id/:car_number', checkSession, userController.addUserCar);
+router.post('/deleteCar/:car_number', checkSession, userController.deleteUserCar);
 
 // user login 추가
 router.post('/userLogin', userController.userLogin);
